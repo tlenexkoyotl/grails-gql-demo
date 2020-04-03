ff923e1fd3b9e2e96e29485d3db21863b196683d
Rashīd Nexkoyotl <rashidtn1419@gmail.com>
2020-04-03 11:25:25 -0600

    Implemented new GQLEntityNamingConvention bean

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,10 +1,12 @@
 package spring
 
-import mx.tlenexkoyotl.graphql_demo.GraphQLContextBuilder
-import mx.tlenexkoyotl.graphql_demo.UserPasswordEncoderListener
+import mx.tlenexkoyotl.graphql_demo.beans.GQLContextBuilder
+import mx.tlenexkoyotl.graphql_demo.beans.GQLEntityNamingConvention
+import mx.tlenexkoyotl.graphql_demo.beans.UserPasswordEncoderListener
 
 // Place your Spring DSL code here
 beans = {
     userPasswordEncoderListener(UserPasswordEncoderListener)
-    graphQLContextBuilder(GraphQLContextBuilder)
+    graphQLEntityNamingConvention(GQLEntityNamingConvention)
+    graphQLContextBuilder(GQLContextBuilder)
 }
